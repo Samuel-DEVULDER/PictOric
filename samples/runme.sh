bada6be3eb9a160498e41c50a4d702ad9e4c05ab
Samuel-DEVULDER <samuel.devulder@laposte.net>
2020-05-26 00:00:41 +0200

    samples updated on mar. 26 mai 2020 00:00:40

--- samples/runme.sh
+++ samples/runme.sh
@@ -1,106 +1,129 @@
 #!/bin/sh
-
+# -----------------------------------------------------------------------------
 set -e
+cd "`dirname $0`"
 
-cd "`dirname $0`"/..
+# -----------------------------------------------------------------------------
 
-export HOME=$PWD
-export USERPROFILE=`cygpath -s -w "$HOME"`
+ERRATT="0.998 0.500 0.000"
+DITHER="0 -3 2 1"
+RESULT=result.md
 
-if false; then
+# -----------------------------------------------------------------------------
 
-dir=libpipi
-if [ ! -d $dir ]; then mkdir $dir; else rm $dir/* || true; fi
-for n in *.*
+export LC_TIME=en_US.UTF-8
+NUMBER_OF_PROCESSORS=${NUMBER_OF_PROCESSORS:-1}
+if test $NUMBER_OF_PROCESSORS -eq 1; then NUMBER_OF_PROCESSORS=2; fi
+MAKE=${MAKE:-make -j`expr $NUMBER_OF_PROCESSORS - 1`}
+MAKE="$MAKE --no-print-directory"
+
+# -----------------------------------------------------------------------------
+# remove space in file names
+for n in *" "*
 do
-	if test -f "$n"
+	if test -f "$n" 
 	then
-		echo -n "$dir/$n..."
-		if convert "$n"  -resize 240x200 -gravity center -background black -extent 240x200 .pictoric.bmp 2>/dev/null
-		then
-			if cmp -s "$n" "${n%.*}.png"
-			then
-				true
-			else
-				rm "$n"
-				cp .pictoric.bmp "${n%.*}.png"
-			fi
-			tools/pipi.exe .pictoric.bmp -o .pictoric.tap .pictoric.tap -o .pictoric.bmp
-			convert ".pictoric.bmp" "$dir/${n%.*}.png"
-			mv .pictoric.tap "$dir/${n%.*}.tap"
-		fi
-		echo done
+		mv "$n" "${n// /_}"
 	fi
 done
-sleep 10
 
-for att in 0.998 0.707 0.500
+# -----------------------------------------------------------------------------
+# convert picture to png & proper size
+for n in *.*
 do
-	for aic in 0 1 
-	do		
-		for dither in 0 -3 2 
-		do
-			params="aic=$aic, dither_lvl=$dither, err_att=$att"
-		
-			cat >$HOME/.pictoric.lua <<EOF
+	case "$n" in
+	*.md|*.png|*.sh) ;;
+	*) 	if test -f "$n"
+		then
+			convert "$n" -resize 240x200 -gravity center -background black -extent 240x200 "${n%.*}.png" 2>/dev/null || true
+		fi;;
+	esac
+done
+
+# -----------------------------------------------------------------------------
+# generate libpipi versions
+dir=libpipi
+if test ! -d $dir; then mkdir $dir; fi
+TARGET=""; for n in *.png; do TARGET="$TARGET $dir/$n"; done
+echo >>$dir/log.txt "*** Update started on `date`"
+$MAKE $TARGET | tee -a $dir/log.txt
+echo >>$dir/log.txt "*** Update ended on `date`"
+
+# -----------------------------------------------------------------------------
+# generate PictOric versions
+PAIR=.pair
+echo -n >$PAIR
+for aic in 0 1 
+do		
+for dither in $DITHER 
+do
+for att in $ERRATT
+do
+	dir="a${aic}d${dither}e${att}"
+	if test ! -d "$dir"; then mkdir "$dir"; fi
+	
+	echo >>$PAIR "$dir" "aic=$aic, dither_lvl=$dither, err_att=$att"		
+	cat >$dir/.pictoric.lua <<EOF
 return {
+	aic=$aic,
 	dist2_alg = 1,
+	dither_lvl=$dither,
+	err_att=$att,
 	oric_emul = "",
-	save_bmp = 1,
-	$params
+	save_bmp = 1
 }
 EOF
-			
-			dir="$params"
-	
-			if [ ! -d "$dir" ]; then mkdir "$dir"; else rm "$dir"/* || true; fi
-			echo -n >"$dir/log.txt"
-			cp "$HOME/.pictoric.lua" "$dir/"
-			
-			echo ""
-			for n in *.png
-			do
-				cp "$n" .pictoric.pix
-				echo -n "$dir/${n%.*}..." | tee -a "$dir/log.txt"
-				# set -x
-				../tools/winb32/luajit.exe ../PictOric.lua .pictoric.pix 2>&1 1>/dev/null | sed -e  's%.*:\s*\(.*\)[\r\n].*%\1%;s%\s*$%%g'| tee -a "$dir/log.txt"
-				convert .pictoric.tap.bmp "$dir/${n%.*}.png"
-				cp .pictoric.tap "$dir/${n%.*}.tap"
-			done
-			sleep 10
-		done
-	done
+	TARGET=""
+	for n in *.png; do TARGET="$TARGET $dir/$n"; done
+
+	echo >>$dir/log.txt "*** Update started on `date`"
+	$MAKE $TARGET "DIR=$dir"  | tee -a $dir/log.txt
+	echo >>$dir/log.txt "*** Update ended on `date`"
+done
+done
 done
 
-rm .pictoric.* || true
+# -----------------------------------------------------------------------------
+# Generae result file
+echo -n Generating $RESULT..
 
-fi
+LEN=""; for i in {1..56}; do LEN="$LEN&nbsp;"; done
 
-LEN=""
-for i in {1..56}; do LEN="$LEN&nbsp;"; done
-
-HLINE="--|--"
-HEADER="$LEN<br>source image<br>$LEN | $LEN<br>\`libpipi\`<br>$LEN"
-for i in *=* 
-do 
-	HLINE="$HLINE|--"
-	HEADER="$HEADER | \`PictOric\`<br>$LEN<br>${i// /<br>}</div>"
+echo  >$RESULT    "# Here is a comparison of various settings on various pictures"
+echo >>$RESULT
+echo >>$RESULT "Click on picture to download the TAP file."
+echo >>$RESULT
+echo >>$RESULT -n "$LEN<br>source image<br>$LEN | $LEN<br>\`libpipi\`<br>$LEN"
+cat $PAIR | while read d t
+do
+	echo >>$RESULT -n " | \`PictOric\`<br>$LEN<br>${t// /<BR>}"
 done
+echo >>$RESULT
 
-cat >result.md <<EOF
-# Here is a comparison of various settings on various pictures
-$HEADER
-$HLINE
-EOF
+echo >>$RESULT -n "--|--"
+cat $PAIR | while read d t
+do
+	echo >>$RESULT -n "|--"
+done
+echo >>$RESULT
 
 for n in *.png
 do
-	LINE="<a href="./$n"><img width=240 height=200	src=\"./$n\" title=\"$n\"></a>"
-	LINE="$LINE | <a href=\"./libpipi/${n%.*}.tap\"><img width=240 height=200 src=\"./libpipi/$n\"></a>"
-	for d in *=*
-	do	
-		LINE="$LINE | <a href=\"./$d/${n%.*}.tap\"><img width=240 height=200 src=\"./$d/$n\" title=\"$d\"></a>"
+	# echo -n ${n%.*}...
+	echo >>$RESULT -n "<a href="./$n"><img width=240 height=200	src=\"./$n\" title=\"$n\"></a>"
+	prev="libpipi/$n"
+	echo >>$RESULT -n "| <a href=\"./libpipi/${n%.*}.tap\"><img id=\"$prev\" width=240 height=200 src=\"./$prev\"></a>"
+	cat $PAIR | while read d t
+	do
+		echo >>$RESULT -n "| <a href=\"./$d/${n%.*}.tap\"><img id=\"$d/$n\" width=240 height=200 src=\"./$d/$n\" title=\"$t\" onmousover=\"$prev.src='./libpipi/$n';\" onmouseout=\"$prev.src='./$d/$n';\"></a>"
+		prev="$d/$n"
 	done
-	echo >>result.md $LINE
+	echo >>$RESULT
 done
-echo >>result.md "(Click to get the TAP file.)"
+echo >>$RESULT
+echo >>$RESULT "Generated on: " `date`
+echo done
+
+# -----------------------------------------------------------------------------
+# cleanup
+rm $PAIR
